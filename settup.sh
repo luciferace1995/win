41486be0e494b97bc36035bd36bed24f854998d7
luciferace1995 <157180390+luciferace1995@users.noreply.github.com>
2024-01-20 23:17:04 +0700

    Update settup.sh

--- settup.sh
+++ settup.sh
@@ -32,15 +32,15 @@ fi
 sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs
 # Downloading resources
 sudo mkdir /mediabots /floppy /virtio
-link1_status=$(curl -Is https://iso.cloudmini.net/0:/Windows_Server_2012_R2x64.ISO | grep HTTP | cut -f2 -d" " | head -1)
-link2_status=$(curl -Is https://iso.cloudmini.net/0:/Windows_Server_2012_R2x64.ISO | grep HTTP | cut -f2 -d" ")
-#sudo wget -P /mediabots https://iso.cloudmini.net/0:/Windows_Server_2012_R2x64.ISOz # Windows Server 2012 R2 
+link1_status=$(curl -Is https://iso2.cloudmini.net/0:/Windows_Server_2012_R2x64.ISO | grep HTTP | cut -f2 -d" " | head -1)
+link2_status=$(curl -Is https://iso2.cloudmini.net/0:/Windows_Server_2012_R2x64.ISO | grep HTTP | cut -f2 -d" ")
+#sudo wget -P /mediabots https://iso2.cloudmini.net/0:/Windows_Server_2012_R2x64.ISO # Windows Server 2012 R2 
 if [ $link1_status = "200" ] ; then 
-	sudo wget -O /mediabots/WS2012R2.ISO http://drive.muavps.net/windows/Windows2012r2.gz 
+	sudo wget -O /mediabots/WS2012R2.ISO https://iso2.cloudmini.net/0:/Windows_Server_2012_R2x64.ISO 
 elif [ $link2_status = "200" -o $link2_status = "301" -o $link2_status = "302" ] ; then 
-	sudo wget -P /mediabots https://iso.cloudmini.net/0:/Windows_Server_2012_R2x64.ISO
+	sudo wget -P /mediabots https://ia601506.us.archive.org/4/items/WS2012R2/WS2012R2.ISO
 else
-	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows OS image urls are available , please report about this issue on Github page : ${NC}https://github.com/luciferace1995/win/"
+	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows OS image urls are available , please report about this issue on Github page : ${NC}https://github.com/mediabots/Linux-to-Windows-with-QEMU"
 	echo "Exiting.."
 	sleep 30
 	exit 1
